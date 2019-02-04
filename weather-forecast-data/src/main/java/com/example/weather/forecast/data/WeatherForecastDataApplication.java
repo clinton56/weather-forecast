92f768fb8cbac7c5ad77e378d2ff202c221d68e1
clinton56 <gigo@openmailbox.org>
2019-02-04 15:46:01 +0100

    Creates skeleton for data persistnce service.

---
+++ weather-forecast-data/src/main/java/com/example/weather/forecast/data/WeatherForecastDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather.forecast.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastDataApplication.class, args);
+    }
+
+}
+
---
+++ weather-forecast-data/src/test/java/com/example/weather/forecast/data/WeatherForecastDataApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.weather.forecast.data;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@ContextConfiguration
+@SpringBootTest
+public class WeatherForecastDataApplicationTests extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
